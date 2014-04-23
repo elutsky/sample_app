3e817ab8a0762f48c16137d91aecdac24634c198
evgeny-the-best <evgenylu@etoro.com>
2014-04-23 10:11:02 +0300

    basic user model + pwd

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-#  get "static_pages/home"
-#  get "static_pages/help"
-#  get "static_pages/about"
-#  get "static_pages/contact"
 
-get "users/new"
+  # get "users/new"
 
-  root :to => 'static_pages#home' 
+  resources :users
+  root :to => 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
